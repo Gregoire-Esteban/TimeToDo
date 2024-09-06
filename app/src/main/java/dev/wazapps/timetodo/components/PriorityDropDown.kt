e996fe8573b7c5ee2c455644c482c72fe5a087e1
Grégoire Esteban <gregoire.esteban@hotmail.fr>
2024-09-06 15:46:03 +0200

    Code cleanup (62.)

--- app/src/main/java/dev/wazapps/timetodo/components/PriorityDropDown.kt
+++ app/src/main/java/dev/wazapps/timetodo/components/PriorityDropDown.kt
@@ -7,32 +7,31 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberCompositionContext
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.PopupProperties
 import dev.wazapps.timetodo.R
 import dev.wazapps.timetodo.components.drawable.Pin
 import dev.wazapps.timetodo.data.models.Priority
@@ -52,8 +51,12 @@ fun PriorityDropDown(
         targetValue = if (expanded) 180f else 0f,
         label = "rotation"
     )
+    var parentSize by remember { mutableStateOf(IntSize.Zero) }
     Row(modifier = modifier
         .fillMaxWidth()
+        .onGloballyPositioned { layoutCoordinates ->
+            parentSize = layoutCoordinates.size
+        }
         .height(PRIORITY_TOP_DOWN_HEIGHT)
         .clickable { expanded = !expanded }
         .border(
@@ -74,31 +77,26 @@ fun PriorityDropDown(
             contentDescription = stringResource(id = R.string.acc_drop_down)
         )
         DropdownMenu(
-            modifier = Modifier.fillMaxWidth(fraction = 0.94f),
+            modifier = Modifier.width(
+                with(LocalDensity.current) {
+                    parentSize.width.toDp()
+                }
+            ),
             expanded = expanded,
             onDismissRequest = { expanded = false },
         ) {
-            DropdownMenuItem(
-                text = { PriorityItem(priority = Priority.LOW) },
-                onClick = {
-                    expanded = false
-                    onPrioritySelected(Priority.LOW)
+            Priority.entries
+                .minus(Priority.NONE)
+                .forEach {
+                    priority ->
+                    DropdownMenuItem(
+                        text = { PriorityItem(priority = priority) },
+                        onClick = {
+                            expanded = false
+                            onPrioritySelected(priority)
+                        }
+                    )
                 }
-            )
-            DropdownMenuItem(
-                text = { PriorityItem(priority = Priority.MEDIUM) },
-                onClick = {
-                    expanded = false
-                    onPrioritySelected(Priority.MEDIUM)
-                }
-            )
-            DropdownMenuItem(
-                text = { PriorityItem(priority = Priority.HIGH) },
-                onClick = {
-                    expanded = false
-                    onPrioritySelected(Priority.HIGH)
-                }
-            )
         }
     }
 }
