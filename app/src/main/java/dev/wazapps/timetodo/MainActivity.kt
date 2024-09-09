73a8b3c669e73ec4dfe339d1c8032232003a187c
Grégoire Esteban <gregoire.esteban@hotmail.fr>
2024-09-09 17:02:48 +0200

    Adjustments to launcher logo & added splashscreen !

--- app/src/main/java/dev/wazapps/timetodo/MainActivity.kt
+++ app/src/main/java/dev/wazapps/timetodo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,9 +17,9 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
     private val sharedViewModel : TaskSharedViewModel by viewModels()
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             TimeToDoTheme {
                 navController = rememberNavController()
